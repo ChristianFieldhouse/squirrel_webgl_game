3a0b8e8617695e13329825ca4a3c8a85ceecf42c
Christian Fieldhouse <christian.fieldhouse@vivid-q.com>
2022-01-01 22:06:32 +0000

    grass with skybox

--- mysrc.js
+++ mysrc.js
@@ -6,14 +6,27 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const box_size = 0.8;
+const image_path = 'https://media.istockphoto.com/photos/artificial-grass-picture-id506692747?b=1&k=20&m=506692747&s=170667a&w=0&h=x4QDWFznTnLQCEmsvCO4w0sZTDYmTvYvPwYD5DW9Ntg='
+const grass_texture = new THREE.TextureLoader().load( image_path );
+// immediately use the texture for material creation
+const grass_material = new THREE.MeshBasicMaterial( { map: grass_texture } );
 
-function add_cube(x, y, colour){
+function add_cube(x, y, colour, box_size=0.8){
     const geometry = new THREE.BoxGeometry(box_size, box_size, box_size);
     const material = new THREE.MeshBasicMaterial( { color: colour } );
     const cube = new THREE.Mesh( geometry, material );
     cube.name = `tile_${x}_${y}`
-    cube.position.set(x - gridsize/2 + 1/2, y - gridsize/2 + 1/2, - box_size);
+    cube.position.set(x, y, 0);
     scene.add( cube );
+}
+
+function add_grass(x, y, box_size=0.8){
+	const geometry = new THREE.BoxGeometry(box_size, 0.1, box_size); //THREE.PlaneGeometry( 1, 1 );
+	const material = new THREE.MeshBasicMaterial( { color: 0x61ab68 } );
+	const plane = new THREE.Mesh( geometry, grass_material );
+    plane.name = `grass_${x}_${y}`
+    plane.position.set(x, 0, y);
+	scene.add( plane );
 }
 
 function col_cube(x, y, colour){
@@ -30,23 +43,38 @@ const colours = {
     "green": 0x61ab68
 }
 
-const gridsize = 50;
-for (var i = 0; i < gridsize; ++i){
-    for (var j = 0; j < gridsize; ++j){
-        //add_cube(i, j, colours[Math.floor(Math.random() * 5)]);
-        add_cube(i, j, colours["grey"]);
-    }
+function createMaterialArray() {
+  const skyboxImagepaths = [
+	'https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&rect=0%2C292%2C3008%2C1504&q=45&auto=format&w=1356&h=668&fit=crop',
+	'https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&rect=0%2C292%2C3008%2C1504&q=45&auto=format&w=1356&h=668&fit=crop',
+	'https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&rect=0%2C292%2C3008%2C1504&q=45&auto=format&w=1356&h=668&fit=crop',
+	'https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&rect=0%2C292%2C3008%2C1504&q=45&auto=format&w=1356&h=668&fit=crop',
+	'https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&rect=0%2C292%2C3008%2C1504&q=45&auto=format&w=1356&h=668&fit=crop',
+	'https://images.theconversation.com/files/393210/original/file-20210401-13-z6rl6z.jpg?ixlib=rb-1.1.0&rect=0%2C292%2C3008%2C1504&q=45&auto=format&w=1356&h=668&fit=crop'
+  ];
+  const materialArray = skyboxImagepaths.map(image => {
+    let texture = new THREE.TextureLoader().load(image);
+    return new THREE.MeshBasicMaterial({ map: texture, side: THREE.BackSide });
+  });
+  return materialArray;
 }
 
-for (var i = 0; i < 10; ++i){
-    for (var j = 0; j < 10; ++j){
-        col_cube(i, j, colours["green"]);
-    } 
-}
+skyboxGeo = new THREE.BoxGeometry(100, 100, 100);
+skybox = new THREE.Mesh(skyboxGeo, createMaterialArray());
+scene.add(skybox);
 
-const matrix = new Array(gridsize).fill(0).map(() => new Array(gridsize).fill(0));
+//add_cube(0, 0, colours["green"], 1);
+add_grass(0, 0, 1);
+add_grass(1, 0, 1);
+add_grass(-1, 0, 1);
+add_grass(0, 1, 1);
+add_grass(1, 1, 1);
+add_grass(-1, 1, 1)
 
-camera.position.z = 75;
+camera.position.x = 0;
+camera.position.y = 0.4;
+camera.position.z = -2;
+camera.lookAt(0, 0, 0);
 
 const line_material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
@@ -73,16 +101,16 @@ const animate = function () {
 document.onkeydown = function(e) {
     switch (e.keyCode) {
       case 37:
-      camera.position.x -= 0.1;
-      break;
-      case 38:
-      camera.position.y += 0.1;
-      break;
-      case 39:
       camera.position.x += 0.1;
       break;
+      case 38:
+      camera.position.z += 0.1;
+      break;
+      case 39:
+      camera.position.x -= 0.1;
+      break;
       case 40:
-      camera.position.y -= 0.1;
+      camera.position.z -= 0.1;
       break;
     }
 };
